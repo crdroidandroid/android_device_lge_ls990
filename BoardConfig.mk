73071f6f4c717df53111724784b9df228fbd14e4
Paul Keith <javelinanddart@gmail.com>
2018-06-02 09:26:50 +0200

    ls990: Use sdfat for exfat and vfat
    Change-Id: Ifb16dd6282d5f6b2521b5a5c504d5732c14ba9fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/ls990/bluetooth
 BOARD_CUSTOM_BT_CONFIG := device/lge/ls990/bluetooth/vnd_g3.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
+# Extended Filesystem Support
+TARGET_EXFAT_DRIVER := sdfat
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_ls990_defconfig
 TARGET_REQUIRES_BUMP := true
