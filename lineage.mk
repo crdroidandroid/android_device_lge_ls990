fcd3b56ddae1c1fe245bf99d13e9f21a39e400d9
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-09-15 12:05:49 +0200

    ls990: Merge full_ls990.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,33 @@
-$(call inherit-product, device/lge/ls990/full_ls990.mk)
+#
+# Copyright 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from ls990 device
+$(call inherit-product, device/lge/ls990/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+PRODUCT_DEVICE := ls990
 PRODUCT_NAME := lineage_ls990
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-ls990
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
