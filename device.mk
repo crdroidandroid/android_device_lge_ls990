64294faa624c3785e9c20d57cd60422afac26c4a
Aleksander <johnnylittleplanet@gmail.com>
2020-02-02 16:38:11 +0100

    ls990: Don't build librmnetctl

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Radio
-PRODUCT_PACKAGES += \
-    librmnetctl
-
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface
